fd9d002feabe651a8535437fb1dfae6a974c9b09
Berk <berkozel@protonmail.com>
2021-10-05 18:09:10 +0300

    Introduction to Spring Web

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # spring-boot-samples
+
+### Introduction:
+This repository contains some newbie stuff about Spring Boot. Nothing fancy.
+
+
+#### Package Index:
+
+01 - spring-boot-web-intro : Hello-world like package about Spring Boot.
---
+++ spring-boot-web-intro/src/test/java/com/frogwine/sbs/springbootwebintro/SpringBootWebIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frogwine.sbs.springbootwebintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
